0a3ae95f0b45494aaa3d2aecb09aa08251d8020e
Hector Corea <corea331@gmail.com>
2025-07-22 14:44:32 -0300

    registro y login

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic import CreateView
+from django.contrib import messages
+from django.shortcuts import redirect
+from django.urls import reverse
+from .forms import RegisterUserForm
 
-# Create your views here.
+
+class RegisterUser(CreateView):
+    template_name = 'registration/register.html'
+    form_class = RegisterUserForm
+
+    def form_valid(self, form):
+        messages.success(self.request, 'Registro de usuario exitoso. Por favor iniciar sesión.')
+        form.save()
+
+        return redirect('apps.users:register')
+
+class LoginUser(LoginView):
+    template_name = 'registration/login.html'
+    
+    def get_success_url(self):
+        messages.success(self.request, 'Login exitoso.')
+
+        return reverse('apps.users:login')
+
+
+class LogoutUser(LogoutView):
+    template_name = 'registration/logout.html'
+
+    def get_success_url(self):
+        messages.success(self.request, 'Logout exitoso.')
+
+        return reverse('apps.users:logout')
